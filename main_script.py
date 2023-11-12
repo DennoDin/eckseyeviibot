086626b2863fd8de9a250b418a503c54ffb91285
Dustin Tran <dustin.andy.tran@gmail.com>
2023-11-13 03:14:05 +0900

    Create strings file for messages

--- main_script.py
+++ main_script.py
@@ -1,13 +1,12 @@
 import discord
 import os
 from dotenv import load_dotenv
+from main_strings import roll_call_dict, log_dict
 
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-raid_role_call_message = 'This roll call for the next raid day.\nPlease react with your role if you\'re able to join us, a :question:if you\'re unsure, or a :x:if you cannot.'
 
 client = discord.Client(intents=intents)
 
@@ -22,12 +21,15 @@ async def on_message(message):
 
     if message.content.startswith('$roll-call'):
         message_parts = message.content.split('!');
-        print(f'LOG: Roll call executed by {message.author}')
+        message_parts = [word.strip() for word in message_parts]
+        print(f'{log_dict["roll_call_executed"].format(message.author)}')
         if(len(message_parts) == 2):
-            print(f'LOG: Roll call with time sent')
-            await message.channel.send(raid_role_call_message)
+            print(f'{log_dict["roll_call_with_time"]}')
+            roll_call_message = roll_call_dict["base"] + roll_call_dict["time"].format(message_parts[1]) + roll_call_dict["react"]
+            await message.channel.send(roll_call_message)
         else:
-            print(f'LOG: Default roll call message sent')
-            await message.channel.send(raid_role_call_message)
+            print(f'{log_dict["roll_call_default"]}')
+            roll_call_message = roll_call_dict["base"] + roll_call_dict["react"]
+            await message.channel.send(roll_call_message)
 
 client.run(os.getenv("BOT_TOKEN"))
---
+++ main_strings.py
@@ -0,0 +1,11 @@
+roll_call_dict = {
+    "base":  "This roll call for our next raid day.",
+    "time":  " We've scheduled it for <t:{}>",
+    "react": "\nPlease react with your role if you\'re able to join us, a :question:if you\'re unsure, or a :x:if you cannot.",
+}
+
+log_dict = {
+    "roll_call_executed": "LOG: Roll call executed by {}",
+    "roll_call_default":  "LOG: Default roll call message sent",
+    "roll_call_with_time": "LOG: Roll call with time sent",
+}
